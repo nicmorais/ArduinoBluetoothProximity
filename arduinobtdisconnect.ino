80e8a20224f34603b1021c659faf215c2952a767
nicmorais <45640048+nicmorais@users.noreply.github.com>
2019-05-17 13:36:55 -0300

    Update arduinobtdisconnect.ino

--- arduinobtdisconnect.ino
+++ arduinobtdisconnect.ino
@@ -1,10 +1,7 @@
-const int ledPin = 13;
 const int statePin = 9;
-const int piezoPin = 10;
+const int piezoPin = 2;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
   pinMode(statePin, INPUT);
   pinMode(piezoPin, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -12,7 +9,6 @@ void setup() {
 
 void loop() {
   int state = digitalRead(statePin);
-  Serial.println(state);
   if(state == 0) {
     
     digitalWrite(LED_BUILTIN, LOW);
